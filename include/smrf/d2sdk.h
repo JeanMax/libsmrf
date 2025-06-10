974c7eb06a0a8d1a578574dda6a80d28749dc458
JeanMax <mc@rmrf.fr>
2025-06-10 16:05:55 +0200

    Update sdk utils fun

--- include/smrf/d2sdk.h
+++ include/smrf/d2sdk.h
@@ -6,277 +6,209 @@
 #include "sdk/monster.h"
 #include "sdk/object.h"
 
-// PRESETS
-inline bool is_shrine(dword id)  //TODO: optimize these
+inline bool is_shrine(const ObjectInfo *obj_info)
 {
-    return id == 2   // Shrine
-        || id == 77  // Healthorama
-        || id == 81  // Forest Altar
-        || id == 83  // Shrine
-        || id == 84  // HealingWell
-        || id == 85  // HealthShrine
-        || id == 86  // Shrine
-        || id == 93  // ManaShrine
-        || id == 96  // HealthShrine
-        || id == 97  // MagicShrine
-        || id == 109 // Shrine HealthR
-        || id == 111 // Fountain 1
-        || id == 113 // Fountain 2 Well
-        || id == 115 // Fountain 3
-        || id == 116 // Snakewoman Shrine
-        || id == 118 // Fountain 4
-        || id == 120 // Healthshrine Dungeun
-        || id == 123 // Innershrinehell2
-        || id == 124 // Innershrinehell3
-        || id == 130 // Pool Wilderness
-        || id == 132 // Cathedralwell Inside
-        || id == 133 // Shrine
-        || id == 134 // Shrine
-        || id == 135 // Shrine
-        || id == 136 // Shrine
-        || id == 137 // Desertwell Well Desert Tom
-        || id == 138 // Cavewell Caves
-        || id == 150 // Shrine1
-        || id == 151 // Shrine4
-        || id == 164 // Mana Well1
-        || id == 165 // Mana Well2
-        || id == 166 // Mana Well3 Tomb
-        || id == 167 // Mana Well4 Harom
-        || id == 168 // Mana Well5
-        || id == 170 // Jungle Healwell
-        || id == 172 // Health Well/shrine Desert
-        || id == 173 // Mana Well/shrine Desert
-        || id == 184 // Jungle Shrine2
-        || id == 190 // Jungle Shrine3
-        || id == 191 // Jungle Shrine4
-        || id == 197 // Jungleshrine
-        || id == 199 // Mafistoshrine
-        || id == 200 // Mafistoshrine
-        || id == 201 // Mafistoshrine
-        || id == 202 // Mafistomana
-        || id == 206 // Mafistohealth
-        || id == 226 // Outershrinehell
-        || id == 231 // Outershrinehell2
-        || id == 232 // Outershrinehell3
-        || id == 236 // Health Wellforhell
-        || id == 249 // HellManaWell
-        || id == 260 // Holyshrine
-        || id == 275 // Health Shrine
-        || id == 276 // Mana Shrine
-        || id == 277 // Magic Shrine
-        || id == 278 // Mana Shrine
-        || id == 279 // Magic Shrine
-        || id == 280 // Healthwell
-        || id == 281 // Manawell
-        || id == 282 // Shrine
-        || id == 299 // MagicShrine
-        || id == 300 // MagicShrine
-        || id == 301 // HealthWell
-        || id == 302 // ManaWell
-        || id == 303 // MagicShrine
-        || id == 319 // HealthWell
-        || id == 320 // HealthWell
-        || id == 322 // TombWell
-        || id == 325 // MagicShrine
-        || id == 343 // ManaWell
-        || id == 344 // HealthWell
-        || id == 361 // MagicShrine
-        || id == 414 // Shrine
-        || id == 415 // Shrine
-        || id == 421 // Shrine 3
-        || id == 422 // Mana Shrine
-        || id == 423 // Health Shrine
-        || id == 426 // Well
-        || id == 427 // Shrine2
-        || id == 428 // Shrine2
-        || id == 464 // Health Shrine
-        || id == 465 // Mana Shrine
-        || id == 472 // IcecaveShrine2
-        || id == 479 // IcecaveShrine2
-        || id == 483 // ManaShrine
-        || id == 484 // BaalHealthShrine
-        || id == 488 // BaalMagicShrine
-        || id == 491 // ManaShrine
-        || id == 492 // HealthShrine
-        || id == 493 // Well
-        || id == 495 // Shrine3
-        || id == 497 // Shrine3
-        || id == 498 // BaalWell
-        || id == 499 // BaalMagicShrine2
-        || id == 503 // BaalMagicShrine3
-        || id == 509 // TempleMagicShrine
-        || id == 512 // TempleMagicShrine2
-        || id == 513 // TempleWell
-        || id == 519 // IcecaveWell
-        || id == 520 // TempleMagicShrine3
-        || id == 521 // TempleHealthShrine
-        || id == 522 // TempleManaShrine
-        || id == 539; // TempleWaypoint
-        // || id == 12  // Rogue Fountain
-        // || id == 31  // Fountain
+    return obj_info->subClass == SUB_SHRINE
+        || obj_info->subClass == SUB_WELL
+        || obj_info->autoMap == MAP_SHRINE
+        || obj_info->autoMap == MAP_WELL;
 }
 
-inline bool is_quest(dword id)
+inline bool is_waypoint(const ObjectInfo *obj_info)
 {
-    return id == 8   // Tower Tome
-        || id == 17  // StoneAlpha
-        || id == 18  // StoneBeta
-        || id == 19  // StoneGamma
-        || id == 20  // StoneDelta
-        || id == 21  // StoneLambda
-        || id == 22  // StoneTheta
-        || id == 30  // Inifuss Tree
-        || id == 108 // Malus
-        || id == 152 // Holder For Horadric Staff
-        || id == 193 // Lam Esen's Tome
-        || id == 251 // Gidbinn Altar
-        || id == 252 // Gidbinn Decoy
-        || id == 354 // Horadric Cube Chest
-        || id == 355 // Horadric Scroll Chest
-        || id == 356 // Staff Of Kings Chest
-        || id == 357 // Horazon's Journal
-        || id == 376 // Forge Hell
-        || id == 392 // Diablo Seal1
-        || id == 393 // Diablo Seal2
-        || id == 394 // Diablo Seal3
-        || id == 395 // Diablo Seal4
-        || id == 396 // Diablo Seal5
-        || id == 404 // Compelling Orb
-        || id == 405 // Khalim Chest
-        || id == 406 // Khalim Chest
-        || id == 407 // Khalim Chest
-        // || id == 458 // Temple Altar // some random altar in halls of anguish
-        || id == 460 // Drehya Outside Town
-        || id == 462 // Nihlathak Outside Town
-        // || id == 523 //  Blacksmith // it's larzuk forge eh
-        || id == 473 // Caged Fellow
-        || id == 546 // AncientsAltar
-        || id == 558 // Frozen Anya
-        || id == 742; // tristram portal
+    return obj_info->subClass == SUB_WAYPOINT;
 }
 
-inline bool is_waypoint(dword id)
+inline bool is_portal(const ObjectInfo *obj_info)
 {
-    return id == 119 // Waypoint Portal
-        || id == 145 // Waypointi Inner Hell
-        || id == 156 // Waypoint
-        || id == 157 // WildernessWaypoint
-        || id == 237 // Act3Waypoint Town
-        || id == 238 // Waypointh
-        || id == 288 // Waypoint Celler
-        || id == 323 // SewerWaypoint
-        || id == 324 // TravincalWaypoint
-        || id == 398 // PandamoniaWaypoint
-        || id == 429 // Waypoint
-        || id == 398 // PandamoniaWaypoint
-        || id == 402 // VallyWaypoint
-        || id == 494 // BaalWaypoint
-        || id == 496 // WildernessWaypoint
-        || id == 511; // IcecaveWaypoint
+    return obj_info->subClass == SUB_PORTAL
+        || obj_info->autoMap == MAP_PORTAL;
 }
 
-inline bool is_transit(dword id)
+inline bool is_special_chest(const ObjectInfo *obj_info)
 {
-    return id == 59  // Town Portal
-        || id == 74  // Trap Door
-        || id == 60  // Permanent Town Portal
-        || id == 194 // Stairsl
-        || id == 195 // Stairsr
-        // || id == 244 // Sewers
-        // || id == 246 // Sewers
-        || id == 262 // Cathedral
-        || id == 263 // Jail
-        || id == 264 // Jail
-        || id == 265 // Jail
-        || id == 298 // Arcane Portal
-        || id == 342 // Hellgate
-        || id == 366 // Sewer Quest Stairs
-        || id == 367 // Sewer Quest Lever
-        || id == 386 // Stairsr
-        || id == 547 // Worldstone Level1
-        || id == 557 // Baal's Lair
-        || id == 563 // Worldstone Chamber
-        || id == 564 // GlacialCaves Level1
-        || id == 565 // Last Cinematic
-        || id == 566 // Harrogath LastPortal
-        || id == 569; // ThroneOfDest Portal
-        // || id == 189 // Cain Portal
-        // || id == 268 // Wirt's Body
-        // || id == 377 // Portal To Guild
-        // || id == 451 // Hellgate
+    return obj_info->autoMap == MAP_CHEST
+        /* || obj_info->id == OBJ_BANK */
+        || obj_info->id == OBJ_SPECIALCHEST100
+        || obj_info->id == OBJ_CONSOLATIONCHEST
+        || obj_info->id == OBJ_PLACEUNIQUECHEST
+        || obj_info->id == OBJ_PLACERANDOMTREASURECHEST
+        || obj_info->id == OBJ_PLACEARCANETHINGAMAJIG;
 }
 
-inline bool is_interesting_preset(dword id)
+inline bool is_chest(const ObjectInfo *obj_info)
 {
-    return is_quest(id)
-        || is_shrine(id)
-        || is_waypoint(id)
-        || is_transit(id)
-        || id == 267 // Bank
-        || id == 455; // special chest TODO: list them all
+    return obj_info->subClass == SUB_CONTAINER
+        || is_special_chest(obj_info);
 }
 
-// for preset unit with type 1
-inline bool is_uninteresting_unit(dword id)
+inline bool is_quest(const ObjectInfo *obj_info)
 {
-    return id == 149 // rock
-        || id == 152 // rogue
-        || id == 179 // cow
-        || id == 195 // lut gholein stuff
-        || id == 196 // lut gholein tree
-        || id == 197 // lut gholein stuff
-        || id == 203 // lut gholein npc
-        || id == 205 // lut gholein npc
-        || id == 273 // gargoyle trap
-        || id == 294 // kurast stuff
-        || id == 296 // kurast stuff
-        || id == 327 // a2 tomb trap
-        || id == 328 // a2 tomb trap
-        || id == 372 // Bonepile (act2 trap)
-        || id == 359 // iron wolf
-        || id == 432 // a5 door
-        || id == 433 // a5 door
-        || id == 434 // a5 prison barb
-        || id == 435 // a5 tower
-        || id == 497 // a5 tower
-        || id == 498 // a5 catapult
-        // || id == 513 // drehya
-        || id == 516 // a5 tree
-        || id == 517 // a5 flames
-        || id == 522 // barb
-        || id == 524 // a5 door
-        || id == 525 // a5 door
-        || id == 528 // a5 demon hut
-        // || id == 563 // baal tentacle
-        // || id == 564 // baal tentacle
-        // || id == 565 // baal tentacle
-        // || id == 566 // baal tentacle
-        || id == 567 // baal tentacle
-        || id == 568 // injuried barb
-        || id == 569 // injuried barb
-        || id == 570 // injuried barb
-        // || id == 571 // baal crab clone
-        // || id == 575 // worldstone effect
-        || id == 795 // a5 prison barb
-        || id == 822 // fallen stuff
-        || id == 823 // fallen stuff
-        || id == 829 // catapult impact
-        || id == 830 // dead barb
-        || id == 831 // catapult impact
-        || id == 832 // dead barb (pickable)
-        || id == 834 // catapult impact
-        || id == 835 // catapult impact
-        || id == 836 // catapult impact
-        || id == 837; // pindle friends
+    return obj_info->id != OBJ_TOWERTOME
+        && (obj_info->autoMap == MAP_TOME
+        || obj_info->autoMap == MAP_CAIN_STONE
+        || obj_info->autoMap == MAP_INIFUSS_TREE
+        || obj_info->autoMap == MAP_MALUS
+        || obj_info->autoMap == MAP_GIBDIN
+        || obj_info->autoMap == MAP_DIABLO_SEAL
+        || obj_info->autoMap == MAP_COMPELLING_ORB
+        || obj_info->autoMap == MAP_ALTAR
+        || obj_info->autoMap == MAP_ANYA
+        || obj_info->autoMap == MAP_CHEST
+        || obj_info->autoMap == MAP_SEWER
+        || obj_info->id == OBJ_SOULSTONEFORGE // Forge Hell
+        || obj_info->id == OBJ_SEVENTOMBSRECEPTACLE // Holder For Horadric Staff
+        || obj_info->id == OBJ_DREHYAOUTSIDETOWN // Drehya Outside Town
+        || obj_info->id == OBJ_NIHLATHAKOUTSIDETOWN // Nihlathak Outside Town
+        || obj_info->id == OBJ_CAGEDWUSSIE1 // Caged Fellow
+            || is_special_chest(obj_info));
+}
+
+inline bool is_transit(const ObjectInfo *obj_info)
+{
+    return obj_info->autoMap == MAP_TRAP_DOOR
+        || obj_info->autoMap == MAP_SEWER
+        || obj_info->autoMap == MAP_STAIR_LEFT
+        || obj_info->autoMap == MAP_STAIR_RIGHT
+        /* || obj_info->id == OBJ_ACT1CATHEDRALSHRINE */
+        /* || obj_info->id == OBJ_ACT1JAILSHRINE */
+        /* || obj_info->id == OBJ_ACT1JAILHEALTHSHRINE */
+        /* || obj_info->id == OBJ_ACT1JAILMANASHRINE */
+        || obj_info->subClass == SUB_ARCANE
+        || obj_info->id == OBJ_ANCIENTSGATEWAY
+        || obj_info->id == OBJ_MAYBETHELASTONE // Baal's lair
+        || obj_info->id == OBJ_FINALPORTAL
+        || obj_info->id == OBJ_SUMMITDOOR
+        || obj_info->id == OBJ_REALLYTHELASTPORTAL
+        || obj_info->id == OBJ_NOTKIDDINGLASTPORTAL
+        || obj_info->id == OBJ_DUMMYEX107;  // Baal portal
+}
+
+inline bool is_npc(const MonsterInfo *mon_info)
+{
+    return mon_info->npc;
+}
+
+inline bool is_ally(const MonsterInfo *mon_info)
+{
+    return mon_info->threat == THREAT_MINION && mon_info->inTown;
+}
+
+inline bool is_a_threat(const MonsterInfo *mon_info)
+{
+    return mon_info->threat >= THREAT_MONSTER && !is_ally(mon_info);
+}
+
+/**
+ * this is for preset type 1 with dwTxtFileNo > MAX_MONSTER + MAX_SUPER
+ * -> usually boss / champions, but can be other weird preset "monstery" stuff
+ */
+enum WeirdPresetMonsterId {
+    FIRST_WEIRD = MAX_MONSTER + MAX_SUPER - 1,
+    DARK_WOOD_FALLEN,
+    // ?
+    RIVER_STALKER = FIRST_WEIRD + 11,
+    SEWER_STALKER,
+    // ?
+    FALLEN_SHAMAN1 = FIRST_WEIRD + 18,
+    FALLEN_SHAMAN2,
+    // ?
+    CATAPULT_IMPACT1 = FIRST_WEIRD + 22,  // don't trust the name
+    DEAD_BARB,
+    RATMEN_SHAMAN,
+    CATAPULT_IMPACT2,  // don't trust the name
+    CATAPULT_IMPACT3,  // don't trust the name
+    CATAPULT_IMPACT4,  // don't trust the name
+    CATAPULT_IMPACT5,  // don't trust the name
+    CATAPULT_IMPACT6,  // don't trust the name
+    PINDLE_FRIENDS,
+    GREMLIN1,
+    GREMLIN2,
+    PINDLE_FRIENDS2,
+    UNKNOW_MEPH_STUFF0,
+    UNKNOW_MEPH_STUFF1,
+    UNKNOW_MEPH_STUFF2,
+    UNKNOW_MEPH_STUFF3,
+};
+typedef enum WeirdPresetMonsterId  WeirdPresetMonsterId;
+
+
+/**
+ * for beyond super monster (`dwTxtFileNo` preset type 1)
+ * @see `enum WeirdPresetMonsterId`
+ */
+inline bool is_weird_preset_monster(dword id)
+{
+    return id == DARK_WOOD_FALLEN
+        || id == FALLEN_SHAMAN1
+        || id == FALLEN_SHAMAN2
+        || id == RIVER_STALKER
+        || id == SEWER_STALKER
+        || id == DEAD_BARB
+        || id == RATMEN_SHAMAN
+        || id == CATAPULT_IMPACT1
+        || id == CATAPULT_IMPACT2
+        || id == CATAPULT_IMPACT3
+        || id == CATAPULT_IMPACT4
+        || id == CATAPULT_IMPACT5
+        || id == CATAPULT_IMPACT6
+        || id == PINDLE_FRIENDS
+        || id == GREMLIN1
+        || id == GREMLIN2
+        || id == PINDLE_FRIENDS2
+        /* || id == UNKNOW_MEPH_STUFF0 */
+        || id == UNKNOW_MEPH_STUFF1
+        || id == UNKNOW_MEPH_STUFF2
+        || id == UNKNOW_MEPH_STUFF3
+        ;
+    /* return id == 149 // rock */
+    /*     || id == 152 // rogue */
+    /*     || id == 179 // cow */
+    /*     || id == 195 // lut gholein stuff */
+    /*     || id == 196 // lut gholein tree */
+    /*     || id == 197 // lut gholein stuff */
+    /*     || id == 203 // lut gholein npc */
+    /*     || id == 205 // lut gholein npc */
+    /*     || id == 273 // gargoyle trap */
+    /*     || id == 294 // kurast stuff */
+    /*     || id == 296 // kurast stuff */
+    /*     || id == 327 // a2 tomb trap */
+    /*     || id == 328 // a2 tomb trap */
+    /*     || id == 372 // Bonepile (act2 trap) */
+    /*     || id == 359 // iron wolf */
+    /*     || id == 432 // a5 door */
+    /*     || id == 433 // a5 door */
+    /*     || id == 434 // a5 prison barb */
+    /*     || id == 435 // a5 tower */
+    /*     || id == 497 // a5 tower */
+    /*     || id == 498 // a5 catapult */
+    /*     // || id == 513 // drehya */
+    /*     || id == 516 // a5 tree */
+    /*     || id == 517 // a5 flames */
+    /*     || id == 522 // barb */
+    /*     || id == 524 // a5 door */
+    /*     || id == 525 // a5 door */
+    /*     || id == 528 // a5 demon hut */
+    /*     // || id == 563 // baal tentacle */
+    /*     // || id == 564 // baal tentacle */
+    /*     // || id == 565 // baal tentacle */
+    /*     // || id == 566 // baal tentacle */
+    /*     || id == 567 // baal tentacle */
+    /*     || id == 568 // injuried barb */
+    /*     || id == 569 // injuried barb */
+    /*     || id == 570 // injuried barb */
+    /*     // || id == 571 // baal crab clone */
+    /*     // || id == 575 // worldstone effect */
 }
 
 // for preset unit with type 5
-inline bool is_backward_tile(dword id)
+inline bool is_backward_tile(dword id) //TODO: parse tile (type 5) lvlwarp.txt
 {
     return id == 5 //Cave Down
         || id == 8 //Crypt Up
         || id == 11 //Tower to Wilderness
-        || id == 13 //Jail Up
+        || id == 13 //Jail Up : same for jail lvl2 and inner cloister :|
+        || id == 14 //Jail Down
         || id == 16 //Catacombs to Cathedral
         || id == 17 //Catacombs Up
         || id == 21 //Sewer Dock to Town
@@ -290,6 +222,7 @@ inline bool is_backward_tile(dword id)
         || id == 48 //Lair Up
         || id == 52 //Spider to Jungle
         || id == 55 //Dungeon Up
+        || id == 57 //Kurast to Sewer
         || id == 58 //Sewer Up L
         || id == 59 //Sewer Up R
         || id == 62 //Temple Up L
@@ -298,6 +231,7 @@ inline bool is_backward_tile(dword id)
         || id == 66 //Mephisto Up R
         || id == 70 //Lava to Mesa
         || id == 73 //Ice Caves Up
+        || id == 75 //Ice Caves Up
         || id == 78 //Temple Up
         || id == 81; //Baal Temple Up
 }
@@ -353,12 +287,12 @@ typedef enum MonsterFlag  MonsterFlag;
 
 enum UnitType {
     UNIT_PLAYER = 0,
-    UNIT_MONSTER = 1,
-    UNIT_OBJECT = 2,
-    UNIT_MISSILE = 3,
-    UNIT_ITEM = 4,
-    UNIT_TILE = 5,
-    UNIT_MAX = 6
+    UNIT_MONSTER,
+    UNIT_OBJECT,
+    UNIT_MISSILE,
+    UNIT_ITEM,
+    UNIT_TILE,
+    UNIT_MAX,
 };
 typedef enum UnitType  UnitType;
 
@@ -370,7 +304,7 @@ enum PlayerClass {
     CLASS_PAL,
     CLASS_BAR,
     CLASS_DRU,
-    CLASS_ASN,
+    CLASS_ASS,
     CLASS_MAX,
 };
 typedef enum PlayerClass  PlayerClass;
