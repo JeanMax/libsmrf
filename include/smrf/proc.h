974611cc3c086e37ff0c597af2b19ba3c3c36c39
JeanMax <mc@rmrf.fr>
2025-06-16 18:25:09 +0200

    Add base address to maps

--- include/smrf/proc.h
+++ include/smrf/proc.h
@@ -18,15 +18,19 @@ typedef void *t_read_callback(byte *buf, size_t buf_len, ptr_t address, void *da
 #define QUICK_MAX_MAP_LEN 0x8000000
 #define is_valid_ptr__quick(ptr) (IS_ALIGNED(ptr)           \
                                   && ptr >= 0x1000          \
-                                  && ptr <= 0x7fffffffffff)
-
+                                  && ptr <= 0x7f0000000000)
+                                  /* && ptr <= 0x7fffffffffff) */
 
 bool is_valid_ptr(ptr_t ptr);
+bool is_valid_ptr__base(ptr_t ptr);
 
 byte *memsearch(const void *mem, const void *search, size_t mem_len, size_t search_len);
 void *memread(ptr_t start_address, size_t length,
               t_read_callback *on_page_read, void *data);
+void *memreadbase(ptr_t start_address, size_t length,
+                  t_read_callback *on_page_read, void *data);
 void *memreadall(bool quick, t_read_callback *on_page_read, void *data);
+void *memreadallbase(t_read_callback *on_page_read, void *data);
 pid_t readmaps(const char *win_name, bool refresh_win);  //TODO: rename
 
 // TODO: could be static
